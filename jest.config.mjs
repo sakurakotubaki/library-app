fadd403841ac9834fe4c016309571b0f84e86ad1
sakurakotubaki <gitonly543@gmail.com>
2025-10-03 08:09:00 +0900

    fix: jest

---
+++ jest.config.mjs
@@ -0,0 +1,9 @@
+export default {
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  transform: {
+    "^.+\\.ts$": ["ts-jest", { useESM: true }]
+  },
+  testMatch: ["**/tests/**/*.test.ts"]
+};
